654f8f8f0b03277a41652f0880ed6bb9467788ed
Bilal Ahmad <77747451+bilalfatah@users.noreply.github.com>
2025-11-08 01:13:35 +0500

    Update Conn.java

--- src/bankingapp/Conn.java
+++ src/bankingapp/Conn.java
@@ -8,7 +8,7 @@ public class Conn {
 	public Connection getConnection() {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3307/bankapp","root","bitterLikeDeath");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3307/db_name","root","db_password");
 			return con;
 		}
 		catch(Exception e) {
